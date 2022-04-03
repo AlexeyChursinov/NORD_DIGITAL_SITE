1d035503ffef706fcad075519148458b78b352f0
AlexeyChursinov <alex.chursinov@gmail.com>
2022-04-03 20:25:26 +0300

    add animation on index

---
+++ js/main.js
@@ -0,0 +1,90 @@
+$(function() {
+    $('.str').liMarquee({
+        direction: 'left',
+        loop: -1,
+        scrolldelay: 0,
+        scrollamount: 60,
+        runshort: true,
+        hoverstop: false,
+        circular: true
+    });
+    $('.str-2').liMarquee({
+        direction: 'right',
+        loop: -1,
+        scrolldelay: 0,
+        scrollamount: 60,
+        runshort: true,
+        hoverstop: false,
+        circular: true
+    });
+
+    // ================
+
+    lottie.loadAnimation({
+        container: document.getElementById('cube'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: '../img/cube.json'
+    });
+
+    // ===============
+
+    var words = document.getElementsByClassName('word');
+    var wordArray = [];
+    var currentWord = 0;
+
+    words[currentWord].style.opacity = 1;
+    for (var i = 0; i < words.length; i++) {
+        splitLetters(words[i]);
+    }
+
+    function changeWord() {
+        var cw = wordArray[currentWord];
+        var nw = currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
+        for (var i = 0; i < cw.length; i++) {
+            animateLetterOut(cw, i);
+        }
+
+        for (var i = 0; i < nw.length; i++) {
+            nw[i].className = 'letter behind';
+            nw[0].parentElement.style.opacity = 1;
+            animateLetterIn(nw, i);
+        }
+
+        currentWord = (currentWord == wordArray.length - 1) ? 0 : currentWord + 1;
+    }
+
+    function animateLetterOut(cw, i) {
+        setTimeout(function() {
+            cw[i].className = 'letter out';
+        }, i * 80);
+    }
+
+    function animateLetterIn(nw, i) {
+        setTimeout(function() {
+            nw[i].className = 'letter in';
+        }, 340 + (i * 80));
+    }
+
+    function splitLetters(word) {
+        var content = word.innerHTML;
+        word.innerHTML = '';
+        var letters = [];
+        for (var i = 0; i < content.length; i++) {
+            var letter = document.createElement('span');
+            letter.className = 'letter';
+            letter.innerHTML = content.charAt(i);
+            word.appendChild(letter);
+            letters.push(letter);
+        }
+
+        wordArray.push(letters);
+    }
+
+    changeWord();
+    setInterval(changeWord, 4000);
+
+
+
+});
