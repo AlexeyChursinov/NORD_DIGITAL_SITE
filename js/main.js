52cc1c5104c1adbceaf5f343d4f5184a13ae582c
AlexeyChursinov <alex.chursinov@gmail.com>
2022-04-10 20:58:23 +0300

    added licenses slider

--- js/main.js
+++ js/main.js
@@ -127,6 +127,7 @@ $(function() {
         pagination: {
             el: ".swiper-pagination",
         },
+        grabCursor: true,
     });
 
     var swiperEmp = new Swiper(".swiperEmps", {
@@ -136,8 +137,51 @@ $(function() {
             el: ".swiper-pagination",
         },
         navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
+            nextEl: "#swiperNextEmp",
+            prevEl: "#swiperPrevEmp",
         },
+        grabCursor: true,
     });
+
+    // const swiperPrevEmp = document.getElementById('swiperPrevEmp')
+    // const swiperNextEmp = document.getElementById('swiperNextEmp')
+
+    // swiperPrevEmp.addEventListener('click', () => {
+    //     mySwiper.slidePrev();
+    // })
+    // swiperNextEmp.addEventListener('click', () => {
+    //     mySwiper.slideNext();
+    // })
+
+    var swiperLicenses = new Swiper(".swiperLicenses", {
+        pagination: {
+            el: ".swiper-pagination",
+            type: 'bullets',
+        },
+        grabCursor: true,
+        breakpoints: {
+            768: {
+                pagination: {
+
+                    el: ".swiper-pagination-licenses",
+                    type: "fraction",
+                },
+                navigation: {
+                    nextEl: "#swiperNext",
+                    prevEl: "#swiperPrev",
+                },
+            }
+        }
+    });
+
+    // const swiperPrev = document.getElementById('swiperPrev')
+    // const swiperNext = document.getElementById('swiperNext')
+
+    // swiperPrev.addEventListener('click', () => {
+    //     mySwiper.slidePrev();
+    // })
+    // swiperNext.addEventListener('click', () => {
+    //     mySwiper.slideNext();
+    // })
+
 });
